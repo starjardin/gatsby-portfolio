a95b5a20236dbf19a6764109b7d2b7d7da230db3
Tantely <tantely.and@onja.org>
2021-07-24 14:28:24 +0300

    remove linkedi =n and twitter link

--- src/components/userLinks.js
+++ src/components/userLinks.js
@@ -4,7 +4,7 @@ const UserLink = props => {
   function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
-  const [github, twitter, linkedIn] = props.link
+  const [github] = props.link
   return (
     <div>
       <h2>Stay in touch</h2>
@@ -15,22 +15,6 @@ const UserLink = props => {
         className={footerStyles.link}
       >
         <span>{capitalize(github.label)}</span>
-      </a>
-      <a
-        href={`https://github.com/${github.username}`}
-        className={footerStyles.link}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <span>{capitalize(twitter.label)}</span>
-      </a>
-      <a
-        href={`https://github.com/${github.username}`}
-        className={footerStyles.link}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <span>{capitalize(linkedIn.label)}</span>
       </a>
     </div>
   )
